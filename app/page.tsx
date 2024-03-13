416a97bcb02a9440e99d67e5a209efc202d82b06
Emre Lutfi <mail@emrelutfi.com>
2024-03-14 02:48:55 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button"
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <Button className={'bg-black text-white hover:bg-gray-600'} variant="destructive">Destructive</Button>
+
+
+    </main>
+  );
+}
